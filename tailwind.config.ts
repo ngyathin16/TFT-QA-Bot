294b5e52f79c0834a5f7afc16aab91964cc55efb
Harold Ng <haroldng2001@gmail.com>
2025-08-11 18:03:39 -0700

    fix vercel build: use JS Tailwind Config and re-export from TS to avoid type error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,45 +1,5 @@
 import type { Config } from "tailwindcss";
-import {
-  default: flattenColorPalette,
-} from "tailwindcss/lib/util/flattenColorPalette";
+import jsConfig from "./tailwind.config.js";
 
-const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  darkMode: "class",
-  theme: {
-    extend: {
-      animation: {
-        aurora: "aurora 60s linear infinite",
-      },
-      keyframes: {
-        aurora: {
-          from: {
-            backgroundPosition: "50% 50%, 50% 50%",
-          },
-          to: {
-            backgroundPosition: "350% 50%, 350% 50%",
-          },
-        },
-      },
-    },
-  },
-  plugins: [addVariablesForColors],
-};
-
-// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
-function addVariablesForColors({ addBase, theme }: any) {
-  let allColors = flattenColorPalette(theme("colors"));
-  let newVars = Object.fromEntries(
-    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
-  );
-
-  addBase({
-    ":root": newVars,
-  });
-}
-
-export default config; 
+const config: Config = jsConfig as unknown as Config;
+export default config;
